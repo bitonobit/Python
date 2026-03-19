da848a5c9671f797f66add01fe5c08ae4b7c7dbb
Bitonobit <bitonobit@gmail.com>
2026-03-19 16:58:58 +0000

    Agregar archivos de ejemplo para la introducción a Python, incluyendo un saludo y el uso de variables.

---
+++ Tutorial/01_comenzar.py
@@ -0,0 +1,10 @@
+print("Hola Mundo!")  #Mensaje de salida
+nombre=input("¿Cuál es tu nombre?") #Entrada de datos
+print("Hola, qué tal estás " + nombre +"?")
+""" Esto es un comentario
+de varias
+líneas
+"""
+'''También lo puedes
+hacer con comillas
+simples'''
---
+++ Tutorial/02_variables.py
@@ -0,0 +1,16 @@
+# Python no tiene ningún comando para declarar una variable, las variables se crean cuando le asignas un valor y pueden cambiar de tipo facilmente
+a=5
+b="Candy"
+c='Candy'
+
+#Python te permite asignar valores a múltiples variables en una sola línea:
+x, y, z = "Bitonobit", "Python", "Candy"
+print(x)
+print(y)
+print(z)
+#Otra forma de hacerlo es:
+print(x +" "+ y +" "+ z)
+
+#Anidar comillas
+print("It's alright")
+print('También se puede citar "Ser o no ser ..."')
